dcb3420e88b5524103a917f3629fe26034fa4f02
Alarlar <52812499+Alarlar@users.noreply.github.com>
2024-12-11 16:00:23 -0800

    homework 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import './App.css';
-import React, { useState } from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import React, { useState, useEffect } from 'react';
+
+function useSemiPersistentState(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
 
 function App() {
-    const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState('todoList', []);
 
-    const addTodo = (newTodo) => {
-        setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
-    };
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
 
-    return (
-        <div>
-            <h1>My Todo List</h1>
-            <AddTodoForm onAddTodo={addTodo} />
-            <TodoList todoList={todoList} />
-        </div>
-    );
+  const removeTodo = (index) => {
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <AddTodoForm addTodo={addTodo} />
+      <TodoList todos={todoList} onRemove={removeTodo} />
+    </div>
+  );
 }
 
 export default App;
