3584dd031b1ae3cd71387c5a23e171b676b53340
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-01-11 19:42:39 -0800

    lesson 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import './App.css';
 import { useState } from 'react';
 import TodoList from './TodoList';
@@ -5,23 +6,56 @@ import AddTodoForm from './AddTodoForm';
 
 
 const App = () => {
+
   const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const addTodo = (newTodo) => {
-    setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
-   };
+  
+  useEffect(() => {
+    const fetchData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem('todoList')) || [],
+          },
+        });
+      }, 2000);
+    });
 
-    const removeTodo = (id) => {
-      setTodoList(todoList.filter(todo => todo.id !== id));
-    };
+    fetchData.then((result) => {
+      setTodoList(result.data.todoList); 
+      setLoading(false); 
+    });
+  }, []);
 
-    return (
+  useEffect(() => {
+    if (!loading) {
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+    }
+  }, [todoList, loading]);
+
+
+  function addTodo(newTodo) {
+      setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
+    }
+
+  const removeTodo = (id) => {
+    setTodoList(todoList.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <>
+      <h1>My Todo List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <>
-            <h1>My Todo List</h1>
-            <AddTodoForm onAddTodo={addTodo} />
-            <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
-        </> // Фрагмент
-    );
+          <AddTodoForm onAddTodo={addTodo} />
+          <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default App;
