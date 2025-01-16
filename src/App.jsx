6835fd7fd3efcfaef5c2b706ee7f0e6348786268
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-01-15 19:37:10 -0800

    lesson_8

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import './App.css';
 import { useState, useEffect } from 'react';
@@ -7,8 +8,38 @@ import AddTodoForm from './AddTodoForm';
 const App = () => {
   
   const [todoList, setTodoList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}', {
+      method: 'GET',
+      headers: {
+        Authorization:`Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`, 
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Error: ${response.status}');
+    }
+
+    const data = await response.json();
+    
+    const todos = data.records.map((record) => ({
+      title: record.fields.title,
+      id: RTCEncodedVideoFrame.id,
+    }));
+    
+    console.log(todos);
+
+    setTodoList(todos);
+
+    setIsLoading(false);
+  } catch (error) {
+    console.error('Fetch error: ${error.message}');
+  }
+};
+  
 
   useEffect(() => {
     const fetchData = new Promise((resolve) => {
